c89ab74b4871a8ce5cacb8d7b7e43b3119cf0b85
nana <adjeibohyen1@gmail.com>
2020-03-29 21:54:38 +0100

    forms undate

--- form_page_zygzyg/form.js
+++ form_page_zygzyg/form.js
@@ -4,8 +4,12 @@ var navSection = document.getElementById("nav-section");
 var body = document.getElementsByTagName("body");
 var aboutLink = document.getElementsByClassName("rec-links");
 var aboutBox = document.getElementsByClassName("about-box");
-var aboutBoxClose = document.querySelector(".about-box-close")
+var aboutBoxClose = document.querySelector(".about-box-close");
+var sigupLink = document.querySelector(".signup-link");
+var signupForm = document.getElementById("signup-form")
+var formBoxClose = document.querySelector(".form-box-close")
 
+// about popup box controls
 aboutLink[0].addEventListener("mouseover", showAboutMenu);
 aboutBoxClose.addEventListener("click", hideAboutMenu);
 
@@ -17,4 +21,16 @@ function showAboutMenu(){
 function hideAboutMenu(){
     popupBox.setAttribute("style", "display: none");
     aboutBox[0].classList.remove("open-about");
+}
+
+// sigup form popup controls
+sigupLink.addEventListener("click", showSignupForm);
+formBoxClose.addEventListener("click", hideSignupForm);
+
+function showSignupForm(){
+    signupForm.classList.add("signup-form-popup");
+}
+
+function hideSignupForm(){
+    signupForm.classList.remove("signup-form-popup");
 }
