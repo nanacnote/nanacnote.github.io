151a12220fa49873592b1259548a6c3d1329c4ca
nana <adjeibohyen1@gmail.com>
2020-04-13 21:41:54 +0100

    final version pending ajax request

--- product_page_sickgallery/deploy/product.js
+++ product_page_sickgallery/deploy/product.js
@@ -1,65 +1,155 @@
 // variables for element selectors
 var clickable_item = document.querySelectorAll(".clickable-item");
 var major_linksPopup = document.querySelector(".major-nav-links-info");
-var minor_linksPopup = document.querySelector(".minor-nav-links-info");
+var major_linksPopup_content = document.querySelector(".nav-links-info-container");
 var check_out_link = document.querySelectorAll(".checkout-basket-header, .checkout-basket-footer");
 var product_picture = document.querySelectorAll(".grid-item>img, .grid-item>.product-info>div>p:nth-child(4)");
 var buy_button = document.querySelectorAll(".buy-button");
 var socials_icons = document.querySelectorAll(".instagram-icon, .facebook-icon");
 
+// all variables below product product page
+var menu_icon = document.querySelector(".menu-icon-wrapper");
+var menu_icon_transition = document.querySelectorAll(".menu-icon");
+var sandwich_popup = document.querySelector(".sandwich-icon-popup-menu");
+
 
 // all function below
 function showPopupMajor(){
     // major links show popup
+    function linksContent(e){
+        let temp = '<div class="nav-links-info-header">ghh</div><div class="nav-links-info-body">ghhg</div>';
+        
+        if(e === "Artist"){
+            return temp;
+        }
+        if(e === "Collection"){
+            return temp;
+        }
+        if(e === "Products"){
+            return temp;
+        }
+        if(e === "Shipping & Returns"){
+            return temp;
+        }
+        if(e === "Terms & Conditions"){
+            return temp;
+        }
+        if(e === "About Us"){
+            return temp;
+        }
+    };
+
+    major_linksPopup.classList.add("is-visible");
+
+    if(this.textContent === "Artist"){
+        major_linksPopup_content.classList.remove("is-visible")
+        major_linksPopup_content.innerHTML = linksContent(this.textContent);
+        major_linksPopup_content.classList.add("is-visible")
+    }
+    if(this.textContent === "Collection"){
+        major_linksPopup_content.classList.remove("is-visible")
+        major_linksPopup_content.innerHTML = linksContent(this.textContent);
+        major_linksPopup_content.classList.add("is-visible")
+    }
+    if(this.textContent === "Products"){
+        major_linksPopup_content.classList.remove("is-visible")
+        major_linksPopup_content.innerHTML = linksContent(this.textContent);
+        major_linksPopup_content.classList.add("is-visible")
+    }
+    if(this.textContent === "Shipping & Returns"){
+        major_linksPopup_content.classList.remove("is-visible")
+        major_linksPopup_content.innerHTML = linksContent(this.textContent);
+        major_linksPopup_content.classList.add("is-visible")
+    }
+    if(this.textContent === "Terms & Conditions"){
+        major_linksPopup_content.classList.remove("is-visible")
+        major_linksPopup_content.innerHTML = linksContent(this.textContent);
+        major_linksPopup_content.classList.add("is-visible")
+    }
+    if(this.textContent === "About Us"){
+        major_linksPopup_content.classList.remove("is-visible")
+        major_linksPopup_content.innerHTML = linksContent(this.textContent);
+        major_linksPopup_content.classList.add("is-visible")
+    }
+    
+}
+function hidePopupMajor(){
+    // major links hide popup
     major_linksPopup.classList.toggle("is-visible");
 }
-function showPopupMinor(){
-    // minor links show popup
-    minor_linksPopup.classList.toggle("is-visible");
-}
-function hidePopup(){
-    //hide popup
-    major_linksPopup.classList.remove("is-visible");
-    minor_linksPopup.classList.remove("is-visible");
-}
 
-// add click listener to clickable objects and perform relevant task
+
+
+
+// _______________________________________________________________
+// select the target node
+var target = document.querySelector(".menu-icon"); 
+// create an observer instance
+var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        // console.log(mutation.type, mutation.attributeName,mutation.oldValue);
+        if (mutation){
+            // console.log(sandwich_popup.classList)
+            if (sandwich_popup.classList.contains("is-visible")){
+                major_linksPopup.style.width = "65%";
+            }
+        }
+    });
+});
+// configuration of the observer:
+var config = { attributes: true, childList: true, characterData: true }
+// pass in the target node, as well as the observer options
+try {
+    observer.observe(target, config);
+  }catch(error) {
+    console.log(error);
+  }
+// later, you can stop observing
+// observer.disconnect();
+// ___________________________________________________________
+
+
+
+// PRODUCT PAGE
+// ________________________________________________________
+
+// add click listener to clickable objects and perform relevant task using textContent selector
 for(i=0; i<clickable_item.length; i++){
-    // console.log(i, clickable_item[i].innerText,clickable_item[i])
+    // console.log(i, clickable_item[i].textContent,clickable_item[i]);
     clickable_item[i].style.cursor = "pointer";
 
-    if (clickable_item[i].innerText === "CLOSE"){
-        clickable_item[i].addEventListener("click", hidePopup);
-    }
-
-    if (clickable_item[i].innerText === "SICK GALLERY"){
+    if (clickable_item[i].textContent === "SICK GALLERY"){
         clickable_item[i].addEventListener("click", function(){
             window.location="product.html"
         });
     }
-    if(clickable_item[i].innerText === "Artist"){
-        clickable_item[i].addEventListener("click", showPopupMajor);
+    if(clickable_item[i].textContent === "Artist"){
+        clickable_item[i].addEventListener("mouseover", showPopupMajor);
+        clickable_item[i].addEventListener("click", hidePopupMajor);
     }
-    if(clickable_item[i].innerText === "Artist"){
-        clickable_item[i].addEventListener("click", showPopupMajor);
+    if(clickable_item[i].textContent === "Collection"){
+        clickable_item[i].addEventListener("mouseover", showPopupMajor);
+        clickable_item[i].addEventListener("click", hidePopupMajor);
     }
-    if(clickable_item[i].innerText === "Collection"){
-        clickable_item[i].addEventListener("click", showPopupMajor);
+    if(clickable_item[i].textContent === "Products"){
+        clickable_item[i].addEventListener("mouseover", showPopupMajor);
+        clickable_item[i].addEventListener("click", hidePopupMajor);
     }
-    if(clickable_item[i].innerText === "Products"){
-        clickable_item[i].addEventListener("click", showPopupMajor);
+    if(clickable_item[i].textContent === "show all"){
+        clickable_item[i].addEventListener("mouseover", showPopupMajor);
+        clickable_item[i].addEventListener("click", hidePopupMajor);
     }
-    if(clickable_item[i].innerText === "show all"){
-        clickable_item[i].addEventListener("click", showPopupMajor);
+    if(clickable_item[i].textContent === "Shipping & Returns"){
+        clickable_item[i].addEventListener("mouseover", showPopupMajor);
+        clickable_item[i].addEventListener("click", hidePopupMajor);
     }
-    if(clickable_item[i].innerText === "Shipping & Returns"){
-        clickable_item[i].addEventListener("click", showPopupMinor);
+    if(clickable_item[i].textContent === "Terms & Conditions"){
+        clickable_item[i].addEventListener("mouseover", showPopupMajor);
+        clickable_item[i].addEventListener("click", hidePopupMajor);
     }
-    if(clickable_item[i].innerText === "Terms & Conditions"){
-        clickable_item[i].addEventListener("click", showPopupMinor);
-    }
-    if(clickable_item[i].innerText === "About Us"){
-        clickable_item[i].addEventListener("click", showPopupMinor);
+    if(clickable_item[i].textContent === "About Us"){
+        clickable_item[i].addEventListener("mouseover", showPopupMajor);
+        clickable_item[i].addEventListener("click", hidePopupMajor);
     }
 }
 
@@ -95,3 +185,21 @@ for(i=0; i<socials_icons.length; i++){
         })
     }
 } 
+
+
+// PRODUCT PRODUCT PAGE
+// _________________________________________________________
+
+// sandwich popup on hover
+for(i=0; i<clickable_item.length; i++){
+    if (clickable_item[i].isSameNode(menu_icon)){
+        clickable_item[i].addEventListener("click", () => {
+            sandwich_popup.classList.toggle("is-visible");
+            menu_icon_transition[0].classList.toggle("is-transition");
+            menu_icon_transition[1].classList.toggle("is-transition");
+            menu_icon_transition[2].classList.toggle("is-transition");
+            major_linksPopup.classList.remove("is-visible");
+        });
+    }
+}
+
